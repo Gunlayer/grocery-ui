dfca4207c4e81afb88c94176815a055c7f22d7fc
Serghei Dedcov <Serghei.Dedcov@endava.com>
2021-10-28 15:50:06 +0000

    Merged PR 57: font family changed
    font family changed

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,7 @@ import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
   typography: {
-    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    fontFamily: "'Open Sans', sans-serif",
   },
   palette: {
     mode: 'light',
